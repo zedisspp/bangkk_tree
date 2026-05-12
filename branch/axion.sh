52b219ea17af6402a8031c55007086a105b2b8ff
Zediss <juniordesigner077@gmail.com>
2026-05-12 10:45:55 -0300

    Change git clone URLs in axion.sh
    Updated repository URLs for various components to use zedisspp instead of doxyey.

--- branch/axion.sh
+++ branch/axion.sh
@@ -1,28 +1,28 @@
 #/bin/bash
 
 #device
-git clone https://$MY_TOKEN@github.com/doxyey/android_device_motorola_bangkk.git -b axion device/motorola/bangkk
+git clone https://$MY_TOKEN@github.com/zedisspp/android_device_motorola_bangkk.git -b axion device/motorola/bangkk
 
 #device-common
-git clone https://$MY_TOKEN@github.com/doxyey/android_device_motorola_sm6375-common.git -b los-erofs device/motorola/sm6375-common
+git clone https://$MY_TOKEN@github.com/zedisspp/android_device_motorola_sm6375-common.git -b los-erofs device/motorola/sm6375-common
 
 #vendor-common
-git clone https://$MY_TOKEN@github.com/doxyey/proprietary_vendor_motorola_sm6375-common.git -b lineage-23.2 vendor/motorola/sm6375-common
+git clone https://$MY_TOKEN@github.com/zedisspp/proprietary_vendor_motorola_sm6375-common.git -b lineage-23.2 vendor/motorola/sm6375-common
 
 #vendor
 git clone https://gitlab.com/ZedissPp/vendor_motorola_bangkk.git vendor/motorola/bangkk
 
 #kernel
-git clone https://$MY_TOKEN@github.com/doxyey/Sedona-android-kernel_motorola_sm6375.git -b baklava kernel/motorola/sm6375
+git clone https://$MY_TOKEN@github.com/zedisspp/Sedona-android-kernel_motorola_sm6375.git -b baklava kernel/motorola/sm6375
 
 #hardware
-git clone https://$MY_TOKEN@github.com/doxyey/android_hardware_motorola.git -b lineage-23.2 hardware/motorola
+git clone https://$MY_TOKEN@github.com/zedisspp/android_hardware_motorola.git -b lineage-23.2 hardware/motorola
 
 #nfc
-git clone https://$MY_TOKEN@github.com/doxyey/android_hardware_samsung_slsi_nfc.git -b lineage-23.2 hardware/samsung/slsi/nfc
+git clone https://$MY_TOKEN@github.com/zedisspp/android_hardware_samsung_slsi_nfc.git -b lineage-23.2 hardware/samsung/slsi/nfc
 
 # Dolby
-git clone https://$MY_TOKEN@github.com/doxyey/vendor_sony_dolby.git -b sixteen vendor/sony/dolby
+git clone https://$MY_TOKEN@github.com/zedisspp/vendor_sony_dolby.git -b sixteen vendor/sony/dolby
 
 # Sign Keys
-git clone https://$MY_TOKEN@github.com/doxyey/vendor_lineage-priv_keys.git -b master vendor/lineage-priv/keys
+git clone https://$MY_TOKEN@github.com/zedisspp/vendor_lineage-priv_keys.git -b master vendor/lineage-priv/keys
